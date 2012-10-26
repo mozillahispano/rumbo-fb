5910ad552f9424a836bd3dfe18cd34bdcb4d0967
johan <gonzalezjohan21@gmail.com>
2012-10-26 11:30:00 -0430

    Nuevo Diseño

--- galeria.php
+++ galeria.php
@@ -28,7 +28,7 @@ $user = $facebook->getUser();
 
 
 $tra= new Trabajo;
-$mostrar=$tra->mostrar_img();
+$mostrar=$tra->mostrarImg();
 
 
 ?> 
@@ -38,13 +38,13 @@ $mostrar=$tra->mostrar_img();
         <meta charset="utf-8"/>
         <title>..::Rumbo | Galeria..::</title>
         <link rel="stylesheet" href="css/estilos.css" type="text/css"/>
-       <link href='http://fonts.googleapis.com/css?family=Lemon' rel="stylesheet" />
+        <link rel="stylesheet" href="css/reveal.css">
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.min.js"></script>
+	<script type="text/javascript" src="js/jquery.reveal.js"></script>
     </head>
     <body>
         <section>
         <?php
-
- 
 
 $urlAuth  =  "http://www.facebook.com/dialog/oauth?client_id=153053701464197&redirect_uri=http://apps.facebook.com/demorumbo/&scope=read_stream,user_status,publish_stream,email";
 $signed_request = $_REQUEST['signed_request']; 
@@ -61,7 +61,7 @@ if ( empty ( $data['user_id'] ) )
 }else
 { 
         
-    
+   
         
 ?>
       
@@ -70,65 +70,60 @@ if ( empty ( $data['user_id'] ) )
 	</header>
 	
             <article>
-                    
+                
 			
-                        <ul id="album">
+                        <ul class="lb-album">
                             <?php 
                             
                             for($i=0;$i<sizeof($mostrar);$i++)
-                            { 
+                           { 
                             ?>
-                            
                             <li>
-                                <div id="detalle">
-                                    <?php  $autor = $facebook->api('/'.$mostrar[$i]["uid"]); ?>
-                                     <h3> Por:<?php  echo $autor["name"];?></h3>
+                            
+                                <a href="#"  class="big-link" data-reveal-id="image-<?php  echo $mostrar[$i]["id_imagenes"]; ?>" data-animation="fade">
+                                        <img src="portadas/thumbnail/<?php  echo $mostrar[$i]["nombre"]; ?>"  />
+
+                                </a>
+
+                                <div   class="reveal-modal"  id="image-<?php  echo $mostrar[$i]["id_imagenes"]; ?>" >
+
+                                        <img src="portadas/<?php  echo $mostrar[$i]["nombre"]; ?>" width="851" height="315" />
+
+                                        <div>
+                                                    <?php  $autor = $facebook->api('/'.$mostrar_transito[$i]["uid"]); ?>
+                                                    <h3><?php  echo $autor["name"];?> <span>Autor</h3>
+                                                    <p><iframe src="https://www.facebook.com/plugins/like.php?href=https://apps.facebook.com/demorumbo/galeria.php?image-<?php echo $mostrar[$i]["id_imagenes"]; ?>" scrolling="no" frameborder="0" style="height: 42px; width: 240px" allowTransparency="true"></iframe>
+                                                    </p>
+                                                    <p>
+                                                    <div id="fb-root"></div>
+                                        <script src="http://connect.facebook.net/es_ES/all.js#xfbml=1"></script>
+                                        <fb:send href="https://apps.facebook.com/demorumbo/galeria.php?image-<?php  echo $mostrar[$i]["id_imagenes"]; ?>"></fb:send>  
+                                                    </p>
+                                    </div>
+                                        <a class="close-reveal-modal">&#215;</a>
+                                          
                                 </div>
-                                <img src="portadas/<?php echo $mostrar[$i]["nombre"]; ?>" />
-                                <div id="social">
-                                     <ul>
-                                         <li>
-                                                <iframe src="https://www.facebook.com/plugins/like.php?href=https://apps.facebook.com/demorumbo/galeria.php?image-<?php // echo $mostrar[$i]["id_imagenes"]; ?>" scrolling="no" frameborder="0" style="height: 42px; width: 250px" allowTransparency="true"></iframe>
-                                           </li>
-                                         
-                                           <li>
-                                                <div id="fb-root"></div>
-                                                <script src="http://connect.facebook.net/es_ES/all.js#xfbml=1"></script>
-                                                <fb:send href="https://apps.facebook.com/demorumbo/galeria.php?image-<?php // echo $mostrar[$i]["id_imagenes"]; ?>"></fb:send>
-                                                <br>
-                                                <br>
-                                           </li>
-                                           
-                                     </ul> 
-                                </div>
-                                
-                                
-                                
-                               </li> 
-                              
+                            
+                            </li>
+                         
                             <?php
                             }
                             ?>
                       </ul>                          
-                 
-		</article>
-           
-	</div>
-      
-           
-          
-	
         
-        
+            </article>
              
         </section>
+       
+        
     </body>
 <?php
 
- } 
+} 
 
 ?>
     
     
 </html>
+
 
